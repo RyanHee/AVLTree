917bea7d264c4615a409fe480a465e12adeb5214
Ryan <111530605+RyanHee@users.noreply.github.com>
2024-02-10 15:57:40 -0600

    Add files via upload

---
+++ AVLTree/src/AVLTreeDriver.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+@SuppressWarnings("unchecked")
+public class AVLTreeDriver {
+    public static void main(String args[])throws IOException
+    {
+        Frame frame = new Frame("AVL Tree");
+    }
+}
