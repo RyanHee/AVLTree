9e2fe29f29af28b1588add957b40e8882bd00b9d
Ryan <111530605+RyanHee@users.noreply.github.com>
2024-02-12 10:07:10 -0600

    Update AVLTree.java

--- AVLTree/src/AVLTree.java
+++ AVLTree/src/AVLTree.java
@@ -94,146 +94,107 @@ public class AVLTree extends BinarySearchTree{
         if (removenode==null){
             return null;
         }
-        //BinaryNode successor = super.successor(removenode);
-        Stack<BinaryNode> ancestors = super.findAllAncestors(i);
-        Stack<BinaryNode> descendants = new Stack<>();
-        BinaryNode temp = removenode;
-        if (temp.right()!=null){
-            temp=temp.right();
-            descendants.add(temp);
+
+        Stack<BinaryNode>stack=new Stack<>();
+        BinaryNode retNode=null;
+        if (removenode.left()!=null && removenode.right()!=null){
+            BinaryNode successor = super.successor(removenode);
+            BinaryNode succParent= super.parent(successor.value());
+            System.out.println(succParent.value());
+            retNode=super.remove(i);
+            stack = super.findAllAncestors(succParent.value());
         }
-
-        while (temp.left()!=null){
-            temp=temp.left();
-            descendants.add(temp);
-        }
-
-        System.out.println("ancestors: "+ancestors);
-
-
-        //() remove node  () remove node
-        //  \               \
-        //   ()              () successor
-        //  /
-        //() successor
-        // there will be no node at the
-        // successor's current spot after deletion, no need to check
-        // there may not be a successor, so check if empty
-        if (!descendants.isEmpty()){
-            descendants.pop();
-        }
-        System.out.println("descendants: "+descendants);
-        BinaryNode retNode =super.remove(i);
-
-        //deleted root
-        if (ancestors.isEmpty()){
-            return retNode;
-        }
-
         else{
-            //() remove node  () remove node
-            //  \               \
-            //   ()a             () successor
-            //  /
-            //() successor
-            //add node a
-            if (ancestors.peek().value().compareTo(retNode.value())<0){
-                if(ancestors.peek().right()!=null){
-                    ancestors.add(ancestors.peek().right());
-                }
-            }
-            else{
-                if(ancestors.peek().left()!=null){
-                    ancestors.add(ancestors.peek().left());
-                }
-            }
-
-
-            while (!descendants.isEmpty()){
-                ancestors.add(descendants.remove(0));
-            }
-
-            Stack<BinaryNode>stack = ancestors;
-            System.out.println("Stack: "+stack);
-            while (!stack.isEmpty()){
-                AVLNode g=(AVLNode) stack.pop();
-                System.out.println(g+" "+g.getBalanceFactor());
-                if (g.getBalanceFactor()==2){
-                    AVLNode left = (AVLNode) g.left();
-                    if (left.getBalanceFactor()!=-1){
-                        //left left rotation;
-                        System.out.println("g: "+g+" l: "+g.left()+" r: "+g.right());
-                        BinaryNode c = leftLeftRotation(g, left);
-                        if (!stack.isEmpty()){
-                            BinaryNode a = stack.pop();
-                            if (a.value().compareTo(g.value())>0){
-                                a.setLeft(c);
-                            }
-                            else{
-                                a.setRight(c);
-                            }
-                        }
-                        else{
-                            super.setRoot(c);
-                        }
-
-                    }
-                    else{
-                        //left right rotation
-                        BinaryNode c = leftRightRotation(g, left, left.right());
-                        if (!stack.isEmpty()){
-                            BinaryNode a = stack.pop();
-                            if (a.value().compareTo(g.value())>0){
-                                a.setLeft(c);
-                            }
-                            else{
-                                a.setRight(c);
-                            }
-                        }
-                        else{
-                            super.setRoot(c);
-                        }
-                    }
-                }
-                else if (g.getBalanceFactor()==-2){
-                    AVLNode right = (AVLNode) g.right();
-                    if (right.getBalanceFactor()==1){
-                        //right left rotation
-                        BinaryNode c = rightLeftRotation(g, right, right.left());
-                        if (!stack.isEmpty()){
-                            BinaryNode a = stack.pop();
-                            if (a.value().compareTo(g.value())>0){
-                                a.setLeft(c);
-                            }
-                            else{
-                                a.setRight(c);
-                            }
-                        }
-                        else{
-                            super.setRoot(c);
-                        }
-                    }
-                    else{
-                        //right right rotation
-                        BinaryNode c = rightRightRotation(g, right);
-                        if (!stack.isEmpty()){
-                            BinaryNode a = stack.pop();
-                            System.out.println("a: "+a);
-                            if (a.value().compareTo(g.value())>0){
-                                a.setLeft(c);
-                            }
-                            else{
-                                a.setRight(c);
-                            }
-                        }
-                        else{
-                            super.setRoot(c);
-                        }
-                    }
-                }
-            }
-
+            BinaryNode parent = super.parent(removenode.value());
+            retNode=super.remove(i);
+            stack=super.findAllAncestors(parent.value());
         }
+
+        //BinaryNode successor = super.successor(removenode);
+
+
+
+
+        System.out.println("Stack: "+stack);
+        while (!stack.isEmpty()){
+            AVLNode g=(AVLNode) stack.pop();
+            System.out.println(g+" "+g.getBalanceFactor());
+            if (g.getBalanceFactor()==2){
+                AVLNode left = (AVLNode) g.left();
+                if (left.getBalanceFactor()!=-1){
+                    //left left rotation;
+                    //System.out.println("g: "+g+" l: "+g.left()+" r: "+g.right());
+                    BinaryNode c = leftLeftRotation(g, left);
+                    if (!stack.isEmpty()){
+                        BinaryNode a = stack.pop();
+                        if (a.value().compareTo(g.value())>0){
+                            a.setLeft(c);
+                        }
+                        else{
+                            a.setRight(c);
+                        }
+                    }
+                    else{
+                        super.setRoot(c);
+                    }
+
+                }
+                else{
+                    //left right rotation
+                    BinaryNode c = leftRightRotation(g, left, left.right());
+                    if (!stack.isEmpty()){
+                        BinaryNode a = stack.pop();
+                        if (a.value().compareTo(g.value())>0){
+                            a.setLeft(c);
+                        }
+                        else{
+                            a.setRight(c);
+                        }
+                    }
+                    else{
+                        super.setRoot(c);
+                    }
+                }
+            }
+            else if (g.getBalanceFactor()==-2){
+                AVLNode right = (AVLNode) g.right();
+                if (right.getBalanceFactor()==1){
+                    //right left rotation
+                    BinaryNode c = rightLeftRotation(g, right, right.left());
+                    if (!stack.isEmpty()){
+                        BinaryNode a = stack.pop();
+                        if (a.value().compareTo(g.value())>0){
+                            a.setLeft(c);
+                        }
+                        else{
+                            a.setRight(c);
+                        }
+                    }
+                    else{
+                        super.setRoot(c);
+                    }
+                }
+                else{
+                    //right right rotation
+                    BinaryNode c = rightRightRotation(g, right);
+                    if (!stack.isEmpty()){
+                        BinaryNode a = stack.pop();
+                        System.out.println("a: "+a);
+                        if (a.value().compareTo(g.value())>0){
+                            a.setLeft(c);
+                        }
+                        else{
+                            a.setRight(c);
+                        }
+                    }
+                    else{
+                        super.setRoot(c);
+                    }
+                }
+            }
+        }
+
+
         return retNode;
 
 
